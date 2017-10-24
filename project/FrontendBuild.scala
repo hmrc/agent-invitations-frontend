5ea5d302ba19387218f06525c17b2ecd28662448
Jenkins HMRC <jenkins-hmrc@digital.cabinet-office.gov.uk>
2017-10-24 15:14:16 +0000

    Creating new service agent-invitations-frontend

---
+++ project/FrontendBuild.scala
@@ -0,0 +1,27 @@
+import sbt._
+import play.sbt.PlayImport._
+import play.core.PlayVersion
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object FrontendBuild extends Build with MicroService {
+
+  val appName = "agent-invitations-frontend"
+
+  override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
+
+  val compile = Seq(
+    ws,
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "8.8.0"
+  )
+
+  def test(scope: String = "test") = Seq(
+    "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
+    "org.scalatest" %% "scalatest" % "2.2.6" % scope,
+    "org.pegdown" % "pegdown" % "1.6.0" % scope,
+    "org.jsoup" % "jsoup" % "1.8.1" % scope,
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+  )
+
+}
