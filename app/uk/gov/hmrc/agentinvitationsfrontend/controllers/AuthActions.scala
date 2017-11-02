c1c1e64c24d93ffb1ce6cffd012e2aa22876d2d4
Artur Opala <artur.opala@siili.fi>
2017-11-02 16:35:16 +0000

    [APB-1428][AO] do not check affinity group at all

--- app/uk/gov/hmrc/agentinvitationsfrontend/controllers/AuthActions.scala
+++ app/uk/gov/hmrc/agentinvitationsfrontend/controllers/AuthActions.scala
@@ -20,7 +20,6 @@ import play.api.mvc.{ Request, Result }
 import uk.gov.hmrc.agentmtdidentifiers.model.{ Arn, MtdItId }
 import uk.gov.hmrc.auth.core.AuthProvider.GovernmentGateway
 import uk.gov.hmrc.auth.core._
-import uk.gov.hmrc.auth.core.authorise.Predicate
 import uk.gov.hmrc.auth.core.retrieve.Retrievals.authorisedEnrolments
 import uk.gov.hmrc.http.HeaderCarrier
 
@@ -29,21 +28,20 @@ import scala.concurrent.{ ExecutionContext, Future }
 trait AuthActions extends AuthorisedFunctions {
 
   protected def withAuthorisedAsAgent[A](body: Arn => Future[Result])(implicit request: Request[A], hc: HeaderCarrier, ec: ExecutionContext): Future[Result] =
-    withEnrolledFor("HMRC-AS-AGENT", "AgentReferenceNumber", AffinityGroup.Agent) {
+    withEnrolledFor("HMRC-AS-AGENT", "AgentReferenceNumber") {
       case Some(arn) => body(Arn(arn))
       case None => Future.failed(InsufficientEnrolments("AgentReferenceNumber identifier not found"))
     }
 
   protected def withAuthorisedAsClient[A](body: MtdItId => Future[Result])(implicit request: Request[A], hc: HeaderCarrier, ec: ExecutionContext): Future[Result] =
-    withEnrolledFor("HMRC-MTD-IT", "MTDITID", AffinityGroup.Individual or AffinityGroup.Organisation) {
+    withEnrolledFor("HMRC-MTD-IT", "MTDITID") {
       case Some(mtdItID) => body(MtdItId(mtdItID))
       case None => Future.failed(InsufficientEnrolments("MTDITID identifier not found"))
     }
 
-  protected def withEnrolledFor[A](serviceName: String, identifierKey: String, affinityGroup: Predicate)(body: Option[String] => Future[Result])(implicit request: Request[A], hc: HeaderCarrier, ec: ExecutionContext): Future[Result] = {
+  protected def withEnrolledFor[A](serviceName: String, identifierKey: String)(body: Option[String] => Future[Result])(implicit request: Request[A], hc: HeaderCarrier, ec: ExecutionContext): Future[Result] = {
     authorised(
-      affinityGroup
-        and Enrolment(serviceName)
+      Enrolment(serviceName)
         and AuthProviders(GovernmentGateway))
       .retrieve(authorisedEnrolments) { enrolments =>
         val id = for {
