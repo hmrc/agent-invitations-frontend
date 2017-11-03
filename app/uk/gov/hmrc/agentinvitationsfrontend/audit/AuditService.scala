7659640e6456555dfba26c8beccd562204f73c11
paul.tan <paul.tan@digital.hmrc.gov.uk>
2017-11-03 22:03:11 +0000

    [APB-796][PT]: Tidy up. Added BearerToken and Unit Test

--- app/uk/gov/hmrc/agentinvitationsfrontend/audit/AuditService.scala
+++ app/uk/gov/hmrc/agentinvitationsfrontend/audit/AuditService.scala
@@ -1,54 +1,70 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.agentinvitationsfrontend.audit
 
 import javax.inject.{Inject, Singleton}
 
 import play.api.mvc.Request
 import uk.gov.hmrc.agentinvitationsfrontend.audit.AgentInvitationEvent.AgentInvitationEvent
-import uk.gov.hmrc.agentinvitationsfrontend.models.{AgentInvitationUserInput, Invitation}
+import uk.gov.hmrc.agentinvitationsfrontend.models.AgentInvitationUserInput
 import uk.gov.hmrc.agentmtdidentifiers.model.Arn
 import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.logging.Authorization
+import uk.gov.hmrc.play.audit.AuditExtensions._
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.audit.model.DataEvent
-import uk.gov.hmrc.play.audit.AuditExtensions._
+import uk.gov.hmrc.play.http.logging.MdcLoggingExecutionContext._
 
 import scala.concurrent.Future
 import scala.util.Try
 
 object AgentInvitationEvent extends Enumeration {
-  val agentInvitationSubmitted = Value
+  val AgentInvitationSubmitted = Value
   type AgentInvitationEvent = Value
 }
 
 @Singleton
-class AuditService @Inject()(val auditConnector: AuditConnector){
+class AuditService @Inject()(val auditConnector: AuditConnector) {
 
   def sendAgentInvitationSubmitted(arn: Arn, invitationId: String, agentInvitationUserInput: AgentInvitationUserInput, result: String)(implicit hc: HeaderCarrier, request: Request[Any]): Unit = {
-    auditEvent(AgentInvitationEvent.agentInvitationSubmitted, "agent-invitation-submitted",
-      Seq("result" -> result,
+    auditEvent(AgentInvitationEvent.AgentInvitationSubmitted, "agent-invitation-submitted",
+      Seq(
+        "result" -> result,
         "invitationId" -> invitationId,
         "agentReferenceNumber" -> arn.value,
         "regimeId" -> agentInvitationUserInput.nino.value,
         "regime" -> "HMRC-MTD-IT",
-        "authorization" -> "",
-        "token" -> "")
-    )
+        "authorization" -> hc.authorization.getOrElse(Authorization("")).value,
+        "token" -> ""))
   }
 
-  private[audit] def auditEvent(event: AgentInvitationEvent, transactionName: String, details: Seq[(String, Any)] = Seq.empty)
-                               (implicit hc: HeaderCarrier, request: Request[Any]): Future[Unit] = {
+  private[audit] def auditEvent(event: AgentInvitationEvent, transactionName: String, details: Seq[(String, Any)] = Seq.empty)(implicit hc: HeaderCarrier, request: Request[Any]): Future[Unit] = {
     send(createEvent(event, transactionName, details: _*))
   }
 
-  private def createEvent(event: AgentInvitationEvent, transactionName: String, details: (String, Any)*)
-                         (implicit hc: HeaderCarrier, request: Request[Any]): DataEvent = {
+  private def createEvent(event: AgentInvitationEvent, transactionName: String, details: (String, Any)*)(implicit hc: HeaderCarrier, request: Request[Any]): DataEvent = {
 
     val detail = hc.toAuditDetails(details.map(pair => pair._1 -> pair._2.toString): _*)
     val tags = hc.toAuditTags(transactionName, request.path)
-    DataEvent(auditSource = "agent-invitation-frontend",
+    DataEvent(
+      auditSource = "agent-invitation-frontend",
       auditType = event.toString,
       tags = tags,
-      detail = detail
-    )
+      detail = detail)
   }
 
   private def send(events: DataEvent*)(implicit hc: HeaderCarrier): Future[Unit] = {
