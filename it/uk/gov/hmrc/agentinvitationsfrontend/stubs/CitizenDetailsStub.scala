4a7c9462e11ad260221387a794895b9d36b81fba
Artur Opala <opala.artur@gmail.com>
2018-06-19 10:34:36 +0100

    [APB-2467][AO] add citizen-details connector to get client's name for personal-income-record invitation

---
+++ it/uk/gov/hmrc/agentinvitationsfrontend/stubs/CitizenDetailsStub.scala
@@ -0,0 +1,43 @@
+package uk.gov.hmrc.agentinvitationsfrontend.stubs
+
+import com.github.tomakehurst.wiremock.client.WireMock._
+import uk.gov.hmrc.agentinvitationsfrontend.support.WireMockSupport
+
+trait CitizenDetailsStub {
+  me: WireMockSupport =>
+
+  def givenCitizenDetailsAreKnownFor(nino: String, firstName: String, lastName: String): Unit = {
+    stubFor(get(urlEqualTo(s"/citizen-details/nino/$nino"))
+      .willReturn(
+        aResponse()
+          .withStatus(200)
+          .withBody(
+            s"""{
+               |   "name": {
+               |      "current": {
+               |         "firstName": "$firstName",
+               |         "lastName": "$lastName"
+               |      },
+               |      "previous": []
+               |   },
+               |   "ids": {
+               |      "nino": "$nino"
+               |   },
+               |   "dateOfBirth": "11121971"
+               |}""".stripMargin)))
+  }
+
+  def givenCitizenDetailsReturns404For(nino: String): Unit = {
+    stubFor(get(urlEqualTo(s"/citizen-details/nino/$nino"))
+      .willReturn(
+        aResponse()
+          .withStatus(404)))
+  }
+
+  def givenCitizenDetailsReturns400For(nino: String): Unit = {
+    stubFor(get(urlEqualTo(s"/citizen-details/nino/$nino"))
+      .willReturn(
+        aResponse()
+          .withStatus(400)))
+  }
+}
