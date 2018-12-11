fb0138b1903a91cc901993b41044522663e24847
Paul Tan <paul.tan@digital.hmrc.gov.uk>
2018-12-11 14:09:24 +0000

    [APB-3140][PT]: Fix Not Match Page

--- it/uk/gov/hmrc/agentinvitationsfrontend/controllers/AgentsErrorControllerISpec.scala
+++ it/uk/gov/hmrc/agentinvitationsfrontend/controllers/AgentsErrorControllerISpec.scala
@@ -29,11 +29,31 @@ class AgentsErrorControllerISpec extends BaseISpec with AuthBehaviours {
           "generic.title",
           "There is a problem",
           htmlEscapedMessage("title.suffix.agents")))
-      checkHtmlResultWithBodyText(result, htmlEscapedMessage("not-matched.description"))
-      checkHtmlResultWithBodyText(result, htmlEscapedMessage("not-matched.advice"))
-      checkHtmlResultWithBodyText(result, htmlEscapedMessage("not-matched.button"))
+      checkHtmlResultWithBodyText(result, "The details you entered do not match to the same client.")
+      checkHtmlResultWithBodyText(result, "Check them and try again.")
+      checkHtmlResultWithBodyText(result,"Try again")
       checkHtmlResultWithNotBodyText(result, "Return to your authorisation requests")
 
+      checkHasAgentSignOutLink(result)
+      verifyAuthoriseAttempt()
+    }
+
+    "return 403 for authorised Agent who submitted not matching known facts if they have a session with no basket" in {
+      testAgentMultiAuthorisationJourneyStateCache.save(AgentMultiAuthorisationJourneyState("", Set.empty))
+
+      val result = notMatched(authorisedAsValidAgent(request, arn.value))
+
+      status(result) shouldBe 403
+      checkHtmlResultWithBodyText(
+        result,
+        htmlEscapedMessage(
+          "generic.title",
+          "There is a problem",
+          htmlEscapedMessage("title.suffix.agents")))
+      checkHtmlResultWithBodyText(result, "The details you entered do not match to the same client.")
+      checkHtmlResultWithBodyText(result, "Check them and try again.")
+      checkHtmlResultWithBodyText(result,"Try again")
+      checkHtmlResultWithNotBodyText(result, "Return to your authorisation requests")
       checkHasAgentSignOutLink(result)
       verifyAuthoriseAttempt()
     }
