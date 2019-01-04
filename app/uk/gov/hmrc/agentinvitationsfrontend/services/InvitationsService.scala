f35dedfd818be8c618eaa804ff5ce5a27baa3880
Suresh Mandalapu <suresh.mandalapu@digital.hmrc.gov.uk>
2019-01-04 15:06:40 +0000

    [APB-3137][SM]: removed unwanted val and minor cleanup

--- app/uk/gov/hmrc/agentinvitationsfrontend/services/InvitationsService.scala
+++ app/uk/gov/hmrc/agentinvitationsfrontend/services/InvitationsService.scala
@@ -208,8 +208,7 @@ class InvitationsService @Inject()(
     service: String,
     cache: AgentMultiAuthorisationJourneyStateCache)(
     implicit hc: HeaderCarrier,
-    ec: ExecutionContext): Future[Boolean] = {
-    val x = 3
+    ec: ExecutionContext): Future[Boolean] =
     for {
       hasPendingInvitations <- invitationsConnector
                                 .getAllPendingInvitationsForClient(arn, clientId, service)
@@ -219,7 +218,6 @@ class InvitationsService @Inject()(
       hasPendingInvitationClientIdInJourney <- cache.get.map(cacheItem =>
                                                 cacheItem.requests.map(_.clientId).contains(clientId))
     } yield hasPendingInvitations | (hasPendingInvitationServiceInJourney && hasPendingInvitationClientIdInJourney)
-  }
 
   private def clientInvitationUrl(invitationId: InvitationId, clientId: String, apiIdentifier: String): String =
     s"/agent-client-authorisation/clients/$apiIdentifier/$clientId/invitations/received/${invitationId.value}"
