2838cef99883bfa286f46418c418587191208996
Suresh <suresh.mandalapu@digital.hmrc.gov.uk>
2019-01-17 15:59:21 +0000

    messageKeyForNiOrg = "niorg"

--- app/uk/gov/hmrc/agentinvitationsfrontend/models/Services.scala
+++ app/uk/gov/hmrc/agentinvitationsfrontend/models/Services.scala
@@ -48,7 +48,7 @@ object Services {
 
   val HMRCNIORG = "HMRC-NI-ORG"
   val UTR = "utr"
-  val messageKeyForNiOrg = "utr"
+  val messageKeyForNiOrg = "niorg"
 
   val supportedServices = List(HMRCMTDIT, HMRCPIR, HMRCMTDVAT, HMRCNIORG)
   val supportedTypes = List("ni", "vrn", "utr")
