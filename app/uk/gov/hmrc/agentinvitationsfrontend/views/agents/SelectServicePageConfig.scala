3eef29de178883d7ae0eca7ab488c77a3a6cd80b
Artur Opala <250927+arturopala@users.noreply.github.com>
2019-02-19 09:45:46 +0000

    [APB-3386][MP, AO] refactor select_service and business_select_service to follow PageConfig pattern

---
+++ app/uk/gov/hmrc/agentinvitationsfrontend/views/agents/SelectServicePageConfig.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentinvitationsfrontend.views.agents
+
+import play.api.i18n.Messages
+import uk.gov.hmrc.agentinvitationsfrontend.controllers.FeatureFlags
+import uk.gov.hmrc.agentinvitationsfrontend.models.Services.{HMRCMTDIT, HMRCMTDVAT, HMRCPIR}
+
+case class SelectServicePageConfig(basketFlag: Boolean, featureFlags: FeatureFlags, services: Set[String])(
+  implicit messages: Messages) {
+
+  val enabledPersonalServices: Seq[(String, String)] = {
+    val map = collection.mutable.Map[String, String]()
+
+    if (featureFlags.showPersonalIncome && services.contains(HMRCPIR))
+      map.update(HMRCPIR, Messages("personal-select-service.personal-income-viewer"))
+
+    if (featureFlags.showHmrcMtdIt && services.contains(HMRCMTDIT))
+      map.update(HMRCMTDIT, Messages("personal-select-service.itsa"))
+
+    if (featureFlags.showHmrcMtdVat && services.contains(HMRCMTDVAT))
+      map.update(HMRCMTDVAT, Messages("select-service.vat"))
+
+    map.toSeq
+  }
+
+}
