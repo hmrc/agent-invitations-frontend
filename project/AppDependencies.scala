df43a37918fe47138237d5eeb19ce2b112f39cc2
dg-hmrc-21 <88377883+dg-21-hmrc@users.noreply.github.com>
2023-07-17 17:55:44 +0100

    Upgrade agent-mtd-identifiers version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % bootstrapVer,
     "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "7.7.0-play-28",
     "uk.gov.hmrc"       %% "play-fsm"                   % "0.89.0-play-28",
-    "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "1.10.0",
+    "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "1.13.0",
     "uk.gov.hmrc"       %% "agent-kenshoo-monitoring"   % "5.4.0",
     "uk.gov.hmrc"       %% "play-partials"              % "8.4.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % mongoVersion
