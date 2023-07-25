00c3f31f1a3e8ff575e5001ed94c335d44ad354f
Colm Cavanagh <colm.m.cavanagh@gmail.com>
2023-07-25 10:14:41 +0100

    [CC]Hacked in messages for cbc

--- app/uk/gov/hmrc/agentinvitationsfrontend/journeys/AgentInvitationJourneyModel.scala
+++ app/uk/gov/hmrc/agentinvitationsfrontend/journeys/AgentInvitationJourneyModel.scala
@@ -260,6 +260,7 @@ object AgentInvitationJourneyModel extends JourneyModel with Logging {
                             maybeSubscription <- getCbcSubscription(cbcClient.cbcId)
                             subscription = maybeSubscription.getOrElse(
                               throw new RuntimeException(s"CBC subscription for ${cbcClient.cbcId} not found!"))
+                            _ = println(subscription)
                             adjustedService = if (subscription.isGBUser) Service.Cbc else Service.CbcNonUk
                             clientName = subscription.anyAvailableName.getOrElse(cbcClient.cbcId.value)
                           } yield
