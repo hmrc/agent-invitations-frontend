d60c484e23e2db28b696a5b0b081aa01c83321ec
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2023-11-23 11:42:10 +0000

    tidy [CS] bump deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt._
 
 object AppDependencies {
 
-  private val mongoVersion: String = "1.3.0"
+  private val mongoVersion: String = "1.4.0"
   private val bootstrapVer: String = "7.22.0"
 
   lazy val compileDeps = Seq(
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-28" % bootstrapVer,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc"         % "7.23.0-play-28",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc"         % "7.28.0-play-28",
     "uk.gov.hmrc"        %% "play-fsm"                   % "0.89.0-play-28",
     "uk.gov.hmrc"        %% "agent-mtd-identifiers"      % "1.15.0",
     "uk.gov.hmrc"        %% "agent-kenshoo-monitoring"   % "5.4.0",
